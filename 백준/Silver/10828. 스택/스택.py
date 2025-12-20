09491ade4e0130a1b6ae48201a622e1c6d4e5e48
Kyung-Hyun36 <66782867+Kyung-Hyun36@users.noreply.github.com>
2025-12-21 00:05:39 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+stack = []
+for i in range(N):
+    command = input().split()
+    if command[0] == "push":
+        stack.append(command[1])
+    elif command[0] == "pop":
+        if len(stack) != 0:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == "size":
+        print(len(stack))
+    elif command[0] == "empty":
+        if len(stack) != 0:
+            print(0)
+        else:
+            print(1)
+    else:
+        if len(stack) != 0:
+            print(stack[-1])
+        else:
+            print(-1)
