5acec9a78a5bfcf44e84b3a16aa1d243a3e6117c
Kyung-Hyun36 <66782867+Kyung-Hyun36@users.noreply.github.com>
2025-12-21 22:10:57 +0900

    [level 0] Title: 나머지 구하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = -1;
+    
+    int mod = num1 % num2;
+    
+    answer = mod;
+    
+    return answer;
+}
