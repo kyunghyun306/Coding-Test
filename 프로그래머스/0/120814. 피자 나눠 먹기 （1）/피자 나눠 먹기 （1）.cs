8e714826aa20de2a3ca7f61150bdcbded2c051d9
Kyung-Hyun36 <66782867+Kyung-Hyun36@users.noreply.github.com>
2025-12-26 20:38:12 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.17 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        answer = n / 7;
+        if (n % 7 != 0){
+            answer++;
+        }
+        return answer;
+    }
+}
