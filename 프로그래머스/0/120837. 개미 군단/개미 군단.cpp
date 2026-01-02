4d6998b75aa275a7a9fb092c854609af64f99dcd
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-01-02 19:42:40 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp) {
+    int answer = 0;
+    
+    while(hp > 0){
+        if(hp - 5 >= 0){
+            answer++;
+            hp -= 5;
+        }else if(hp - 3 >= 0){
+            answer++;
+            hp -= 3;
+        }else{
+            answer++;
+            hp--;
+        }
+    }
+    
+    return answer;
+}
