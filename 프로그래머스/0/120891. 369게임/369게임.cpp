643a3e0ca388a2629d5e19cfadff609ad3bd0aca
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-01-13 13:55:26 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    for(char c : to_string(order)){
+        if(c == '3' || c == '6' || c == '9') answer++;
+    }
+    return answer;
+}
