cc5d001382c954c387d874f0a277a74f218d1ba9
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-04-07 13:18:34 +0900

    [level 0] Title: 코드 처리하기, Time: 0.49 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cpp
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 string solution(string code) {
     string answer = "";
-    int mode = 0;
+    bool mode = false;
     
     for(int i = 0; i < code.size(); i++){
         if(code[i] == '1'){
-            (mode ? mode = 0 : mode = 1);
+            mode = !mode;
         }else{
             if(mode){
                 if(i % 2) answer += code[i];
