efa4e87e8fef93d3506729a08d0e9f35844f06d3
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-04-09 14:57:59 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    
+    while(n > 1){
+        answer.push_back(n);
+        (n % 2 ? n = 3 * n + 1 : n = n / 2);
+    }
+    
+    answer.push_back(n);
+    
+    return answer;
+}
